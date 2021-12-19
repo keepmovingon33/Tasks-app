263d93b3162498b6a71343bfe8803d5f6e4c53f9
sky <anvanc37@gmail.com>
2021-12-19 10:20:49 -0500

    setup project

---
+++ TasksApp/TasksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasksApp
+//
+//  Created by sky on 12/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
